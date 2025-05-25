8fa7c25c700ae4a95cb9b46eb15210490355754d
chuch <ilya.chuprunov@gmail.com>
2025-05-25 20:28:05 +0500

    красивости в настройках и виджете добавления

--- flutter_application_1/lib/ui/widgets/add_food_widget.dart
+++ flutter_application_1/lib/ui/widgets/add_food_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/domain/entity/themeviewmodel.dart';
 import 'package:flutter_application_1/ui/widgets/add_food_model.dart';
 import 'package:provider/provider.dart';
 
@@ -8,24 +10,99 @@ class AddFoodWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final VM = context.watch<AddFoodModel>();
+    final themeVM = Provider.of<ThemeViewModel>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Новая позиция'),
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            TextField(controller: VM.nameController,),
-            TextField(controller: VM.caloriesController,),
-            TextField(controller: VM.proteinsController,),
-            TextField(controller: VM.fatsController,),
-            TextField(controller: VM.carbohydratesController,),
-            TextButton(onPressed: () async{
-              await VM.addNewFood();
-              Navigator.pop(context);
-            }, child: Text('добавить'))
-          ],
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          decoration: BoxDecoration(
+            color: themeVM.isDarkTheme ? Colors.black : Colors.white,
+            border: Border.all(color: Colors.grey, width: 2),
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                SizedBox(height: 20,),
+                TextField(controller: VM.nameController,
+                    decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Наименование продукта',
+                    alignLabelWithHint: true, // <-- Это нужно, чтобы label не плавал в центре
+                  ),
+                ),
+                SizedBox(height: 15,),
+                TextField(
+                  controller: VM.caloriesController,
+                  keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly, // <-- только цифры
+                    ],
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Калорийность, гр',
+                    ),
+                ),
+                SizedBox(height: 15,),
+                TextField(controller: VM.proteinsController,
+                  keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly, // <-- только цифры
+                    ],
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Белки, гр',
+                    ),
+                ),
+                SizedBox(height: 15,),
+                TextField(controller: VM.fatsController,
+                  keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly, // <-- только цифры
+                    ],
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Жиры, гр',
+                    ),
+                ),
+                SizedBox(height: 15,),
+                TextField(controller: VM.carbohydratesController,
+                  keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly, // <-- только цифры
+                    ],
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Углеводы, гр',
+                    ),
+                ),
+                SizedBox(height: 15,),
+                ElevatedButton(onPressed: () async{
+                    await VM.addNewFood();
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  shadowColor: Colors.grey,
+                ), 
+                  child: Text('Добавить'),
+                )
+              ],
+            ),
+          ),
         ),
       )
     );
--- flutter_application_1/lib/ui/widgets/settingsFoods_widget.dart
+++ flutter_application_1/lib/ui/widgets/settingsFoods_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/domain/entity/themeviewmodel.dart';
 import 'package:flutter_application_1/ui/widgets/settingsFoods_model.dart';
 import 'package:provider/provider.dart';
@@ -29,27 +30,91 @@ class SettingsFoodsWidget extends StatelessWidget {
               ),
             ],
           ),
-          child: Column(
-            children: [
-              TextField(controller: VM.weightController,),
-              TextField(controller: VM.tallController,),
-              TextField(controller: VM.oneProteinController,),
-              TextField(controller: VM.oneFatsController,),
-              TextField(controller: VM.oneCarControllet,),
-              TextButton(onPressed: () async{
-                if(await VM.checkEditingNULL()==true)
-                {
-                  await VM.saveFoodSettings();
-                  Navigator.pop(context);
-                }
-                else
-                {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Не все данные заполнены!')),
-                  );
-                }
-              }, child: Text('Сохранить'))
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                SizedBox(height: 20,),
+                TextField(
+                  controller: VM.weightController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly, // <-- только цифры
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Вес',
+                  ),
+                ),
+                SizedBox(height: 15,),
+                TextField(
+                  controller: VM.tallController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly, // <-- только цифры
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Рост',
+                  ),
+                ),
+                SizedBox(height: 15,),
+                TextField(
+                  controller: VM.oneProteinController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly, // <-- только цифры
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Грамм белка на один килограмм',
+                  ),
+                ),
+                SizedBox(height: 15,),
+                TextField(
+                  controller: VM.oneFatsController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly, // <-- только цифры
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Грамм жиров на один килограмм',
+                  ),
+                ),
+                SizedBox(height: 15,),
+                TextField(
+                  controller: VM.oneCarControllet,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly, // <-- только цифры
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Грамм углеводов на один килограмм',
+                  ),
+                ),
+                SizedBox(height: 15,),
+                ElevatedButton(onPressed: () async{
+                  if(await VM.checkEditingNULL()==true)
+                  {
+                    await VM.saveFoodSettings();
+                    Navigator.pop(context);
+                  }
+                  else
+                  {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Не все данные заполнены!')),
+                    );
+                  }
+                }, 
+                style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  shadowColor: Colors.grey,
+                ),
+                child: Text('Сохранить'),)
+              ],
+            ),
           ),
         )
       ),
